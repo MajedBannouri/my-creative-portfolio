192ed839df01dd995cfb72b6dc84adf69a1b3db9
MajedBannouri <majed.bannouri12@gmail.com>
2021-02-10 20:45:09 +0100

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div classNme="answer">
+                    <p>lorem ipsum dolor sit amet</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div classNme="answer">
+                    <p>lorem ipsum dolor sit amet</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods </h4>
+                <div classNme="answer">
+                    <p>lorem ipsum dolor sit amet</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What Products do you offer ? </h4>
+                <div classNme="answer">
+                    <p>lorem ipsum dolor sit amet</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AboutUsSection from '../components/AboutSection'
+import FaqSection from '../components/FaqSection'
 import ServicesSection from '../components/ServicesSection'
 
 const AboutUs = () => {
@@ -7,6 +8,7 @@ const AboutUs = () => {
         <div>
         <AboutUsSection />
         <ServicesSection/>
+        <FaqSection></FaqSection>
         </div>
 
     )
