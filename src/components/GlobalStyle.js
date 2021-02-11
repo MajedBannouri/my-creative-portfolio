492e7c54121039dbf0ac9ed71b6bca9cdf4b78b7
MajedBannouri <majed.bannouri12@gmail.com>
2021-02-11 12:53:37 +0100

    6. Styling Services Section

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -9,12 +9,14 @@ const GlobalStyle = createGlobalStyle`
 }
 
 body{
-    background:#1b1b1b
+    background:#1b1b1b;
+    font-family: 'Inter', sans-serif;
 }
 
 button{
     font-weight:bold;
     font-size: 1.1 rem;
+    font-family: 'Inter', sans-serif;
     cursor: pointer;
     padding: 1rem 2rem;
     border: 3px solid #23d997;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -4,52 +4,81 @@ import diaphragm from '../img/diaphragm.svg'
 import money from '../img/money.svg'
 import teamwork from '../img/teamwork.svg'
 import home2 from '../img/home2.png'
+import styled from 'styled-components'
+import {About, Description, Image, Hide} from '../styles'
+
 const ServicesSection = () => {
     return (
-        <div className="services">
-            <div className="description">
+        <Services>
+            <Description>
                 <h2>High <span>quality</span> services</h2>
-                <div className="cards">
-                    <div className="cards">
-                        <div className="card">
+                <Cards>
+                    
+                        <Card>
                             <div className="icon">
                                 <img src={clock} />
                                 <h3>Effecient</h3>
                             </div>
                             <p>lorem ipsum dolor sit amet</p>
-                        </div>
+                        </Card>
                         
-                        <div className="card">
+                        <Card>
                             <div className="icon">
                                 <img src={teamwork} />
                                 <h3>teamwork</h3>
                             </div>
                             <p>lorem ipsum dolor sit amet</p>
-                        </div>
+                        </Card>
                         
-                        <div className="card">
+                        <Card>
                             <div className="icon">
                                 <img src={diaphragm} />
                                 <h3>diaphragm</h3>
                             </div>
                             <p>lorem ipsum dolor sit amet</p>
-                        </div>
+                        </Card>
                         
-                        <div className="card">
+                        <Card>
                             <div className="icon">
                                 <img src={money} />
                                 <h3>Affordable</h3>
                             </div>
                             <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-            <div className="image">
-                <img src={home2} />
-            </div>
-        </div>
+                        </Card>
+                   
+                </Cards>
+            </Description> 
+            <Image>
+                <img src={home2} alt="" />
+            </Image>
+        </Services>
     )
 }
-
+const Services = styled(About)`
+    h2{
+        padding-bottom: 5rem;
+    }
+    p{
+        width:70%;
+        padding: 2rem 0rem 4rem 0rem;
+    }
+`
+const Cards = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+const Card = styled.div`
+    flex-basis: 20rem;
+    .icon{
+        display:flex;
+        align-items: center;
+        h3{
+            margin-left: 1rem;
+            background: white;
+            color: black;
+            padding: 1rem;
+        }
+    }
+    
+`
 export default ServicesSection
