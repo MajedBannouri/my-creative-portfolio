36dc5b5b5065852202e10085565c3a92c6bb1cf3
MajedBannouri <majed.bannouri12@gmail.com>
2021-02-11 13:47:26 +0100

    8. Creating Nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import pages
 import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs'
-
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div >
     <GlobalStyle></GlobalStyle>
+      <Nav></Nav>
       <AboutUs/>
     </div>
   );
